f8acd4c08a62d7409aadb17d7e781031a583bad2
S Soundarya <soundarya.s@tvsnext.com>
2023-08-02 21:56:05 +0530

    [Soundarya]Added Firt name validation code

---
+++ UserRegistration/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace UserRegistrationProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Validate validate = new Validate();
+            Console.WriteLine("Enter the first name to validate:");
+            string FirstName = Console.ReadLine();
+            validate.FirstName(FirstName);
+        }
+    }
+}
---
+++ UserRegistration/Validate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblem
+{
+    public class Validate
+    {
+        string firstName = "^[A-Z]{1}[a-z]{2,}$";
+        public void FirstName(string input)
+        {
+            bool result = Regex.IsMatch(input, firstName);
+            if (result)
+                Console.WriteLine("Validation Successful");
+            else
+                Console.WriteLine("Validation Unsuccessful");
+        }
+    }
+}
